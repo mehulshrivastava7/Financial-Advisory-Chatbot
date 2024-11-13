adeb508916266e165bf379a7f722cbfaaa49ccec
Ramanan14 <119160811+Ramanan14@users.noreply.github.com>
2024-11-14 01:16:49 +0530

    Update optimization.py

--- Optimization_module/optimization.py
+++ Optimization_module/optimization.py
@@ -1,11 +1,13 @@
-"""This code is by Ramanan"""
+"""This code was written by Ramanan"""
 import yfinance as yf
 import pandas as pd
 import numpy as np
 import os
+from scipy.optimize import minimize
+from datetime import datetime, timedelta
  
 # Set the working directory to the project directory
-PROJECT_DIR = "/home/smehul/project/project/"
+PROJECT_DIR = "/home/ramanant/myenv/project/"
 os.chdir(PROJECT_DIR)
  
 def stochastic_optimization(tickers):
@@ -19,81 +21,159 @@ def stochastic_optimization(tickers):
                 indian_tickers.append(ticker)
             elif currency == "USD":
                 us_tickers.append(ticker)
+            elif currency == None:
+                print(f"Couldn't find currency for {ticker}. Skipping...")
         return us_tickers, indian_tickers
- 
+    def timezone(us_tickers,indian_tickers):
+        if len(us_tickers)>0 and len(indian_tickers)>0:
+            ticker1=us_tickers[0]
+            end_date = datetime.now() - timedelta(days=1)
+            start_date = end_date - timedelta(days=30)
+
+# Download data
+            hist = yf.download(ticker1, start=start_date.strftime('%Y-%m-%d'), end=end_date.strftime('%Y-%m-%d')) # 1 month daily data
+            data1=hist[['Close']].rename(columns={'Close': 'Close_'+ticker1})
+            len1=data1.shape[0]
+            ticker2=indian_tickers[0]
+            end_date = datetime.now() - timedelta(days=1)
+            start_date = end_date - timedelta(days=30)
+            hist=yf.download(ticker2, start=start_date.strftime('%Y-%m-%d'), end=end_date.strftime('%Y-%m-%d'))
+            data2=hist[['Close']].rename(columns={'Close': 'Close_'+ticker2})
+            len2=data2.shape[0]
+            if len1!=len2:
+                timezone=1
+            else:
+                timezone=0
+        else:
+            timezone=0
+        return timezone
+
     def get_inr_to_usd_rate():
         inr_usd = yf.Ticker("INRUSD=X").history(period="1d")
+        
         return inr_usd['Close'][0] if not inr_usd.empty else None
- 
-    def get_stock_data(tickers, inr_to_usd_rate):
+
+    def get_stock_data(tickers, inr_to_usd_rate,t):
         market_cap = []
         ticker=tickers[0]
         stock = yf.Ticker(ticker)
         currency = stock.info.get("currency", None)
         market_cap.append(stock.info.get("marketCap", None))
         if currency == "INR" and inr_to_usd_rate:
-            market_cap[0] = market_cap[0] / inr_to_usd_rate
-        hist = stock.history(period="1mo")  # 1 month daily data
+            market_cap[0] = market_cap[0] * inr_to_usd_rate
+        end_date = datetime.now() - timedelta(days=1)
+        start_date = end_date - timedelta(days=30)
+        
+
+# Download data
+        if t==1:
+            if currency=="INR":
+                start_date= start_date - timedelta(days=1)
+            
+        hist = yf.download(ticker, start=start_date.strftime('%Y-%m-%d'), end=end_date.strftime('%Y-%m-%d')) # 1 month daily data
         data=hist[['Close']].rename(columns={'Close': 'Close_'+ticker})
-        data[ticker]=hist['Close'].pct_change().dropna()
+        if currency == "INR" and inr_to_usd_rate:
+            data['Close_'+ticker]=data['Close_'+ticker]*inr_to_usd_rate
+        data[ticker]=data['Close_'+ticker].pct_change()
         data.drop('Close_'+ticker,inplace=True,axis=1)
-        tickers.pop(0)
- 
+        data.reset_index(drop=True)
+        first=tickers.pop(0)
+        
+        i=0
+        
+
         for ticker in tickers:
-            i=0
+            currency = stock.info.get("currency", None)
             stock = yf.Ticker(ticker)
-            hist = stock.history(period="1mo")  # 1 month daily data
-            data[ticker]=hist['Close'].pct_change().dropna()   # Return in percentage
- 
+            end_date = datetime.now() - timedelta(days=1)
+            start_date = end_date - timedelta(days=30)
+            if t==1:
+                if currency=="INR":
+                    start_date= start_date - timedelta(days=1)
+            hist = yf.download(ticker, start=start_date.strftime('%Y-%m-%d'), end=end_date.strftime('%Y-%m-%d')) # 1 month daily data
+            df=hist['Close']# 1 month daily data
+            df.reset_index(drop=True)
+            if currency == "INR" and inr_to_usd_rate:
+                df=df*inr_to_usd_rate
+            df=df.pct_change()
+            data[ticker]=df.values
+             # Return in percentage
+
             # Get daily market capitalization
             market_cap.append(stock.info.get("marketCap", None))
-            currency = stock.info.get("currency", None)
- 
+              
+
             if market_cap[i+1] and currency == "INR" and inr_to_usd_rate:
-                market_cap[i+1]= market_cap[i+1] / inr_to_usd_rate
+                market_cap[i+1]= market_cap[i+1] * inr_to_usd_rate
             elif market_cap[i+1] and currency == "USD":
                 market_cap[i+1] = market_cap[i+1]
+            
             i+=1
- 
- 
-        return data, market_cap
- 
+        data=data.dropna()
+        
+
+        return data, market_cap,first
+
     # Main Execution
     us_tickers, indian_tickers = separate_tickers(tickers)
- 
+    tickers=us_tickers+indian_tickers
+    t=timezone(us_tickers,indian_tickers)
+
     # Fetch the INR to USD conversion rate
     inr_to_usd_rate = get_inr_to_usd_rate()
-    data, market_cap = get_stock_data(tickers, inr_to_usd_rate)
-    returns=data
+    
+    if not inr_to_usd_rate:
+        print("INR to USD rate not available. Exiting...")
+        return
+    data, market_cap,first = get_stock_data(tickers, inr_to_usd_rate,t)
+    tickers.insert(0,first)
+    length=len(market_cap)
+    
+    del_L=[]
+    for i in range(length):
+        if market_cap[i]==None:
+            print(f"Market cap for {tickers[i]} not available. Skipping...")
+            del_L.append(i)
+    for i in del_L:
+        market_cap[i]=0
+        data.drop(tickers[i],inplace=True,axis=1)
+        tickers[i]=0
+    market_cap=[x for x in market_cap if x!=0]
+    tickers=[x for x in tickers if x!=0]
+            
+            
+    
     risk_free_rate = [0,0]
     risk_free_rate[0]=((1+(0.069))**(1/365))-1
     risk_free_rate[1]=((1+(0.049))**(1/365))-1
     risk_free_rate=(risk_free_rate[0]*(len(indian_tickers))+risk_free_rate[1]*(len(us_tickers)))/(len(indian_tickers)+len(us_tickers))
- 
+
     # Sharpe Ratio objective function
     def sharpe_ratio(weights, returns, risk_free_rate):
-        portfolio_return = np.dot(weights, returns.mean())   # Annualized return
+        portfolio_return = np.dot(weights, returns.mean())# Annualized return
         portfolio_std = np.sqrt(np.dot(weights.T, np.dot(returns.cov() , weights)))  # Annualized volatility
         return (portfolio_return - risk_free_rate) / portfolio_std
- 
+
     # Stochastic Gradient Descent for Sharpe Ratio maximization
     def stochastic_sharpe_optimization(market_cap,returns, risk_free_rate, num_iterations=5000, learning_rate=0.002, tolerance=1e-5):
         num_assets = returns.shape[1]
         num_init=10*(num_assets)
         # Start with equal weights
         sum_weights = np.sum(market_cap)
+        
         weights = market_cap/sum_weights
+        
         best_sharpe = sharpe_ratio(weights, returns, risk_free_rate)
         best_weights = weights
         L=[]
- 
+        L.append((best_weights,best_sharpe))
         for i in range(num_iterations):
             # Add a random small perturbation to weights for stochasticity
             perturbation = np.random.normal(0, 0.1, num_assets)
             new_weights = weights + learning_rate * perturbation
             new_weights = np.clip(new_weights, 0, 1)  # Ensure weights are between 0 and 1
             new_weights /= np.sum(new_weights)  # Normalize to sum to 1
- 
+
             # Calculate new Sharpe Ratio
             new_sharpe = sharpe_ratio(new_weights, returns, risk_free_rate)
             
@@ -101,26 +181,29 @@ def stochastic_optimization(tickers):
             if new_sharpe > best_sharpe:
                 best_sharpe = new_sharpe
                 best_weights = new_weights
+                L.append((best_weights,best_sharpe))
+                weights = new_weights
             
             # Update current weights for the next iteration
-            weights = new_weights
- 
+            
+
             # Convergence check
-            if np.abs(new_sharpe - best_sharpe) < tolerance:
-                break
-        L.append((best_weights,best_sharpe))
+            
+
+        
         for i in range(num_init):
             weights = np.random.rand(num_assets)
             weights /= np.sum(weights)
             best_sharpe = sharpe_ratio(weights, returns, risk_free_rate)
             best_weights = weights
+            L.append((best_weights,best_sharpe))
             for i in range(num_iterations):
                 # Add a random small perturbation to weights for stochasticity
                 perturbation = np.random.normal(0, 0.1, num_assets)
                 new_weights = weights + learning_rate * perturbation
                 new_weights = np.clip(new_weights, 0, 1)  # Ensure weights are between 0 and 1
                 new_weights /= np.sum(new_weights)  # Normalize to sum to 1
- 
+
                 # Calculate new Sharpe Ratio
                 new_sharpe = sharpe_ratio(new_weights, returns, risk_free_rate)
                 
@@ -128,40 +211,49 @@ def stochastic_optimization(tickers):
                 if new_sharpe > best_sharpe:
                     best_sharpe = new_sharpe
                     best_weights = new_weights
+                    L.append((best_weights,best_sharpe))
+                    weights = new_weights
                 
                 # Update current weights for the next iteration
-                weights = new_weights
- 
+                
+
                 # Convergence check
-                if np.abs(new_sharpe - best_sharpe) < tolerance:
-                    break
-            L.append((best_weights,best_sharpe))
-        best_sharpe=L[0][1]
- 
+                
+            
+            
+        if len(L)!=0:
+            best_sharpe=L[0][1]
+        else:
+            return [0]*len(market_cap),-1
+
         for i in range(1,len(L)):
             if L[i][1]>best_sharpe:
                 best_sharpe=L[i][1]
                 best_weights=L[i][0]
-        return best_weights, best_sharpe      
+        return best_weights, best_sharpe
+
+
         
- 
+        
+
     # Run optimization
-    optimal_weights, optimal_sharpe = stochastic_sharpe_optimization(market_cap,returns, risk_free_rate)
-    return optimal_weights, optimal_sharpe
+    optimal_weights, optimal_sharpe= stochastic_sharpe_optimization(market_cap,data, risk_free_rate)
+
+    
+    return optimal_weights, optimal_sharpe,tickers
  
 if __name__ == "__main__":
     try:
         df = pd.read_excel(os.path.join(PROJECT_DIR, 'user_data.xlsx'))
-        tickers = df['Stock Ticker'].tolist()
+        tickers = df['Ticker'].tolist()
         print(df['Total Value ($)'])
         total_money = df['Total Value ($)'].sum()
-        optimal_weights, optimal_sharpe = stochastic_optimization(tickers)
+        optimal_weights, optimal_sharpe,tickers = stochastic_optimization(tickers)
         print("sum of weights",sum(optimal_weights))
         print("total money",total_money)
         optimal_money_split = [total_money * weight for weight in optimal_weights]
  
         # Save optimal weights and Sharpe ratio to a CSV file
-        tickers = df['Stock Ticker'].tolist()
         split_df = pd.DataFrame({
             'Ticker': tickers,
             'Optimal Money Split ($)': optimal_money_split
